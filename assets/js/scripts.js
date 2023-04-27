8f9480f90313ccd33aa754c586dd5637b75437b8
verastian <verastian0908@gmail.com>
2023-04-27 19:57:57 -0400

    feature:about section and script to perform smooth-scroll are added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,7 +10,19 @@ $(() => {
         }// fin de la condición
     });
 
-
+    // Agregar smooth scrolling automatico a todos los enlaces con la clase "page-scroll"
+    $(".page-scroll").on('click', function (event) {
+        console.log(this.hash)
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        } // fin de la condición
+    });
 
 });
 
